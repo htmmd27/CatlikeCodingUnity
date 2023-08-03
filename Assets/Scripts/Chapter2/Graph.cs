b834300c9814d95b32d65a4c3e05ae4ace4ad5c2
shidanyang <shidanyang03@kuaishou.com>
2023-08-03 11:41:23 +0800

    chapter2.2.2 Basics/Building a Graph/Visualizing Math

--- Assets/Scripts/Chapter2/Graph.cs
+++ Assets/Scripts/Chapter2/Graph.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class Graph : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Graph : MonoBehaviour {
+
+    [SerializeField]
+    Transform pointPrefab;
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+    Transform[] points;
+    
+    void Awake () {
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = Instantiate(pointPrefab);
+            points[i] = point;
+            position.x = (i + 0.5f) * step - 1f;
+            //position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
+        }
     }
 }
