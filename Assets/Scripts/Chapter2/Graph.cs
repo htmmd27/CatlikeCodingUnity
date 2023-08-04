12898561c982e58003142bda86b0637ac2b33229
shidanyang <shidanyang03@kuaishou.com>
2023-08-04 10:52:26 +0800

    chapter2.2.4 Basics/Measuring Performance/MS and FPS

--- Assets/Scripts/Chapter2/Graph.cs
+++ Assets/Scripts/Chapter2/Graph.cs
@@ -46,12 +46,28 @@ public class Graph : MonoBehaviour {
     private void Update()
     {
         duration += Time.deltaTime;
-        if (duration >= functionDuration) {
+        if (transitioning) 
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration) 
+        {
             duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
             //function = FunctionLibrary.GetNextFunctionName(function);
             PickNextFunction();
         }
-        UpdateFunction();
+        if (transitioning) {
+            UpdateFunctionTransition();
+        }
+        else {
+            UpdateFunction();
+        }
         
         // for (int i = 0; i < points.Length; i++)
         // {
@@ -89,6 +105,27 @@ public class Graph : MonoBehaviour {
             float u = (x + 0.5f) * step - 1f;
             //float v = (z + 0.5f) * step - 1f;
             points[i].localPosition = f(u, v, time);
+        }
+    }
+    
+    void UpdateFunctionTransition () {
+        FunctionLibrary.Function
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(
+                u, v, time, from, to, progress
+            );
         }
     }
 }
